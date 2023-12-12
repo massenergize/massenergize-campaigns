5db95ef6969453838a5d96379c687c31c0496005
Kobina_Koomson <georgeranch31@gmail.com>
2023-12-12 19:53:00 +0000

    feat: Improve sidebar with collapse functionality and new styles
    Added SCSS for sidebar, enhanced the dropdowns in NavigationBars to be collapsible, and created a new SVG for a DownChevron icon to indicate dropdown menus. Also updated routing destinations in menu.ts.

--- src/layouts/admin-layout.tsx
+++ src/layouts/admin-layout.tsx
@@ -1,5 +1,7 @@
 import { SideBar } from "../layout-components/side-bar/side-bar";
 import { Col, Row } from "react-bootstrap";
+import { SWRConfig } from "swr";
+import { fetchData } from "../helpers/utils/http";
 
 interface AdminLayoutProps {
   children: React.ReactNode;
@@ -8,11 +10,13 @@ interface AdminLayoutProps {
 export function AdminLayout(props: AdminLayoutProps) {
   const { children } = props;
   return (
+    <SWRConfig value={{ dedupingInterval : 5000, fetcher : fetchData }}>
     <Row>
       <Col md={"auto"}>
         <SideBar />
       </Col>
       <Col>{children}</Col>
     </Row>
+    </SWRConfig>
   );
 }
