2001605dffc795041667c145c28654d2a078d6b3
frimpongopoku <pongofrimi@gmail.com>
2023-12-14 18:07:45 +0000

    new tweaks to getagreatdeal

--- src/user-portal/pages/technology/GetAGreatDealSection.js
+++ src/user-portal/pages/technology/GetAGreatDealSection.js
@@ -4,14 +4,29 @@ import SectionTitle from "../../../components/pieces/SectionTitle";
 import { Col, Row } from "react-bootstrap";
 
 function GetAGreatDealSection({ sectionId, data, image }) {
-  const { title, description, first_deal, second_deal, third_deal } =
-    data || {};
+  const {
+    title,
+    description,
+    first_deal,
+    second_deal,
+    third_deal,
+    description_2,
+    first_link,
+    second_link,
+    third_link,
+  } = data || {};
+
+  const boxes = [
+    { text: first_deal, link: first_link },
+    { text: second_deal, link: second_link },
+    { text: third_deal, link: third_link },
+  ];
   return (
     <div
       id={sectionId}
       className="mt-5"
       style={{
-        background: "white",
+        // background: "red",
         width: "100%",
         padding: "80px 0px",
         minHeight: 200,
@@ -24,7 +39,7 @@ function GetAGreatDealSection({ sectionId, data, image }) {
           {description}
         </p>
         <div className="" style={{ position: "relative" }}>
-          <img
+          {/* <img
             style={{
               width: "100%",
               height: 350,
@@ -35,22 +50,24 @@ function GetAGreatDealSection({ sectionId, data, image }) {
               image?.url ||
               "https://picsum.photos/id/870/300/300?grayscale&blur=2"
             }
-          />
+          /> */}
           <div
             style={{
               zIndex: 2,
-              marginTop: -240,
-              marginLeft: -102,
+              // marginTop: -240,
+              // marginLeft: -102,
             }}
           >
-            <Row>
-              {[first_deal, second_deal, third_deal].map((item, index) => {
+            <Row style={{ margin: "50px 0px" }}>
+              {/* {[first_deal, second_deal, third_deal].map((item, index) => { */}
+              {(boxes || []).map((item, index) => {
                 if (!item) return <></>;
                 return (
                   <Col
+                    onClick={() => window.open(item?.link, "_blank")}
                     className="elevate-4 touchable-opacity"
                     key={index?.toString()}
-                    lg={{ span: 3, offset: 1 }}
+                    lg={{ span: 3 }}
                     style={{
                       height: 150,
                       background: "white",
@@ -60,14 +77,18 @@ function GetAGreatDealSection({ sectionId, data, image }) {
                       justifyContent: "center",
                       borderRadius: 5,
                       textAlign: "center",
+                      marginRight: 50,
                     }}
                   >
-                    <h3 style={{ margin: 0, color: "red" }}>{item}</h3>
+                    <h3 style={{ margin: 0, color: "red" }}>{item.text}</h3>
                   </Col>
                 );
               })}
             </Row>
           </div>
+          <p style={{ textAlign: "justify" }} className="mb-3">
+            {description_2}
+          </p>
         </div>
       </OptimumWrapper>
     </div>
