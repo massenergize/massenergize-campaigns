d3b912ea91408e8ae656541ff799bad9c2398a4c
frimpongopoku <pongofrimi@gmail.com>
2023-12-15 14:47:01 +0000

    login skeleton now setup

---
+++ src/admin-portal/pages/auth/Login.js
@@ -0,0 +1,161 @@
+import React, { useState } from "react";
+import {
+  Card,
+  Form,
+  Button,
+  Container,
+  Row,
+  Col,
+  InputGroup,
+  ModalFooter,
+  Spinner,
+} from "react-bootstrap";
+import MERichText from "../../../components/admin-components/RichText";
+import Input from "../../../components/admin-components/Input";
+import { validateEmail } from "../../../utils/utils";
+import Notification from "../../../components/pieces/Notification";
+
+function Login({}) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const submit = () => {
+    const emailIsValid = validateEmail(email?.trim());
+    if (!emailIsValid)
+      return setError("Please include a valid email and password");
+
+    
+  };
+
+  return (
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ height: "100vh", padding: 0 }}
+    >
+      <div
+        className="elevate-float-pro"
+        style={{
+          minHeight: "29vh",
+          width: "65vh",
+          background: "white",
+          borderRadius: 6,
+        }}
+      >
+        <div style={{ padding: 30 }}>
+          <div style={{ textAlign: "center" }}>
+            <h3 style={{ color: "var(--admin-theme-color)" }}>
+              Admin Campaign Authentication
+            </h3>
+            <p>Sign in as a campaign administrator to manage your campaigns</p>
+          </div>
+          <div style={{ padding: "10px 20px" }}>
+            <InputGroup className="mb-3">
+              <InputGroup.Text style={{ fontWeight: "bold" }} id="basic-addon1">
+                Email{" "}
+              </InputGroup.Text>
+              <Form.Control
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                type="email"
+                placeholder="Enter email here..."
+                aria-label="Username"
+                aria-describedby="basic-addon1"
+              />
+            </InputGroup>
+            <InputGroup className="mb-3">
+              <InputGroup.Text id="basic-addon1" style={{ fontWeight: "bold" }}>
+                Password{" "}
+              </InputGroup.Text>
+              <Form.Control
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                type="password"
+                placeholder="Enter password here..."
+                aria-label="Username"
+                aria-describedby="basic-addon1"
+              />
+            </InputGroup>
+            <Notification show={error}>{error}</Notification>
+          </div>
+
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              padding: "0px 20px",
+            }}
+          >
+            <div style={{ display: "inline-block", marginLeft: "auto" }}>
+              <Button
+                className="touchable-opacity"
+                style={{
+                  marginRight: 10,
+                  background: "#ee0c0c",
+                  fontWeight: "bold",
+                  borderWidth: 0,
+                }}
+              >
+                Use Gmail Instead
+              </Button>
+              <Button
+                disabled={loading || !email || !password}
+                className="touchable-opacity"
+                style={{
+                  fontWeight: "bold",
+                  background: "var(--admin-theme-color)",
+                  borderWidth: 0,
+                }}
+              >
+                {loading && (
+                  <Spinner size="sm" style={{ marginRight: 5 }}></Spinner>
+                )}
+                <span>Submit</span>
+              </Button>
+            </div>
+          </div>
+        </div>
+        {/* <div
+          style={{
+            background: "grey",
+            display: "flex",
+            flexDirection: "row",
+            margin: 0,
+            borderBottomRightRadius: 6,
+            borderBottomLeftRadius: 5,
+          }}
+        >
+          <Button style={{ marginLeft: "auto" }}>Here we go</Button>
+        </div> */}
+      </div>
+
+      {/* <Row>
+        <Col className="mx-auto">
+          <Card>
+            <Card.Body>
+              <Card.Title className="text-center">Login</Card.Title>
+              <Form>
+                <Form.Group controlId="formBasicEmail">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+
+                <Button variant="primary" type="submit" block>
+                  Submit
+                </Button>
+              </Form>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row> */}
+    </Container>
+  );
+}
+
+export default Login;
--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -3,9 +3,9 @@ import { Route, Routes, useParams } from "react-router-dom";
 import LandingPage from "../user-portal/pages/landing-page/LandingPage";
 import { bindActionCreators } from "redux";
 import {
-	appInnit,
-	testReduxAction,
-	toggleUniversalModal,
+  appInnit,
+  testReduxAction,
+  toggleUniversalModal,
 } from "../redux/actions/actions";
 import { connect } from "react-redux";
 import CustomModal from "../components/modal/CustomModal";
@@ -19,128 +19,133 @@ import { AllTechnologies } from "../admin-portal/pages/technology/all";
 import OneTestimonial from "../user-portal/pages/testimonials/OneTestimonial";
 import CreateCampaignAccount from "../admin-portal/pages/campaign-account/CreateCampaignAccount";
 import { CampaignStatistics } from "../admin-portal/pages/campaign/campaign-statistics/campaign-statistics";
+import Login from "../admin-portal/pages/auth/Login";
 
 const ROUTE_TABLE = [
-	{
-		path: "/technology/:campaign_technology_id",
-		component: TechnologyFullViewPage,
-		addToggleModal: true,
-	},
-	{
-		path: "/technology/event/:eventId",
-		component: OneEvent,
-		addToggleModal: true,
-	},
-	{
-		path: "/technology/testimonial/:id",
-		component: OneTestimonial,
-		addToggleModal: true,
-	},
+  {
+    path: "/technology/:campaign_technology_id",
+    component: TechnologyFullViewPage,
+    addToggleModal: true,
+  },
+  {
+    path: "/technology/event/:eventId",
+    component: OneEvent,
+    addToggleModal: true,
+  },
+  {
+    path: "/technology/testimonial/:id",
+    component: OneTestimonial,
+    addToggleModal: true,
+  },
 
-	{
-		path: "/admin/campaign/new",
-		component: NewCampaign,
-	},
-	{
-		path: "/admin/campaign/create-technology",
-		component: CreateTechnology,
-	},
-	{
-		path: "/admin/campaign/new/preview",
-		component: AllComponents,
-	},
+  {
+    path: "/admin/campaign/new",
+    component: NewCampaign,
+  },
+  {
+    path: "/admin/campaign/create-technology",
+    component: CreateTechnology,
+  },
+  {
+    path: "/admin/campaign/new/preview",
+    component: AllComponents,
+  },
 
-	{
-		path: "/admin/campaign/edit/:id",
-		component: AllComponents,
-	},
-	{
-		path: "/admin/campaign/all",
-		component: AllCampaigns,
-	},
+  {
+    path: "/admin/campaign/edit/:id",
+    component: AllComponents,
+  },
+  {
+    path: "/admin/campaign/all",
+    component: AllCampaigns,
+  },
 
-	{
-		path: "/admin/campaign/:id/stats/",
-		component: CampaignStatistics,
-	},
+  {
+    path: "/admin/campaign/:id/stats/",
+    component: CampaignStatistics,
+  },
 
-	{
-		path: "/admin/campaign-account/new",
-		component: CreateCampaignAccount,
-	},
+  {
+    path: "/admin/campaign-account/new",
+    component: CreateCampaignAccount,
+  },
 
-	{
-		path: "/admin/technology/new",
-		component: CreateTechnology,
-	},
+  {
+    path: "/admin/technology/new",
+    component: CreateTechnology,
+  },
 
-	{
-		path: "/admin/technology/all",
-		component: AllTechnologies,
-	},
+  {
+    path: "/admin/technology/all",
+    component: AllTechnologies,
+  },
 
-	{
-		path: "/admin/campaign-preview/:id",
-		component: CampaignStatistics,
-	},
-	{
-		path: "/components",
-		component: AllComponents,
-	},
+  {
+    path: "/admin/campaign-preview/:id",
+    component: CampaignStatistics,
+  },
+  {
+    path: "/components",
+    component: AllComponents,
+  },
+  {
+    path: "/login",
+    component: Login,
+  },
 ];
 
 function AppRouter({
-	test,
-	testFunction,
-	modalOptions,
-	toggleModal,
-	appInnit,
+  test,
+  testFunction,
+  modalOptions,
+  toggleModal,
+  appInnit,
 }) {
-	const params = useParams();
-	return (
-		<>
-			<CustomModal
-				close={() => toggleModal({ show: false, component: <></> })}
-				{...modalOptions}
-			/>
-			<Routes>
-				<Route
-					path="/:campaignId"
-					element={
-						<LandingPage
-							test={test}
-							testFunction={testFunction}
-							toggleModal={toggleModal}
-						/>
-					}
-				/>
+  const params = useParams();
+  return (
+    <>
+      <CustomModal
+        close={() => toggleModal({ show: false, component: <></> })}
+        {...modalOptions}
+      />
+      <Routes>
+        <Route
+          path="/:campaignId"
+          element={
+            <LandingPage
+              test={test}
+              testFunction={testFunction}
+              toggleModal={toggleModal}
+            />
+          }
+        />
 
-				{ROUTE_TABLE.map((route, index) => {
-					const { path, addToggleModal } = route;
-					const routeProps = {
-						path,
-						element: (
-							<route.component
-								toggleModal={addToggleModal ? toggleModal : null}
-							/>
-						),
-					};
-					return <Route key={index} {...routeProps} />;
-				})}
-			</Routes>
-		</>
-	);
+        {ROUTE_TABLE.map((route, index) => {
+          const { path, addToggleModal } = route;
+          const routeProps = {
+            path,
+            element: (
+              <route.component
+                toggleModal={addToggleModal ? toggleModal : null}
+              />
+            ),
+          };
+          return <Route key={index} {...routeProps} />;
+        })}
+      </Routes>
+    </>
+  );
 }
 
 const mapState = (state) => {
-	return { test: state.testStore, modalOptions: state.modalOptions };
+  return { test: state.testStore, modalOptions: state.modalOptions };
 };
 
 const mapDispatch = (dispatch) => {
-	return bindActionCreators(
-		{ testFunction: testReduxAction, toggleModal: toggleUniversalModal },
-		dispatch
-	);
+  return bindActionCreators(
+    { testFunction: testReduxAction, toggleModal: toggleUniversalModal },
+    dispatch
+  );
 };
 
 export default connect(mapState, mapDispatch)(AppRouter);
