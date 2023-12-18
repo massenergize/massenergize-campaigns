49328fe5562f2c9304dc619de522e547b6985412
frimpongopoku <pongofrimi@gmail.com>
2023-12-18 12:09:20 +0000

    added dev route

--- src/api/urls.js
+++ src/api/urls.js
@@ -4,9 +4,6 @@ export var API_HOST = "http://localhost:8000/api";
 // export var API_HOST = "http://2606-154-160-22-219.ngrok-free.app";
 
 if (IS_PROD) API_HOST = "...";
-if (IS_DEV) API_HOST = "...";
+if (IS_DEV) API_HOST = "https://api.massenergize.dev";
 
-
-
-
-export const CAMPAIGN_INFORMATION_URL = "campaigns.info"
+export const CAMPAIGN_INFORMATION_URL = "campaigns.info";
