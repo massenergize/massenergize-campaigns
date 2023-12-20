1bf550cf52ad3391d8b50e26661e56be9a9e5cff
Kobina_Koomson <georgeranch31@gmail.com>
2023-12-20 21:10:54 +0000

    feat: point api host to dev

--- src/api/urls.js
+++ src/api/urls.js
@@ -1,7 +1,7 @@
 import { IS_DEV, IS_PROD } from "./config";
 
-// export var API_HOST = "https://api.massenergize.dev";
-export var API_HOST = "http://localhost:8000/api";
+export var API_HOST = "https://api.massenergize.dev";
+// export var API_HOST = "http://localhost:8000/api";
 
 if (IS_PROD) API_HOST = "...";
 // if (IS_DEV) API_HOST = "https://api.massenergize.dev";
