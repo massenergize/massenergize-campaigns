151141a52f454a45588afb403a203bdd03bfcc05
Kobina_Koomson <georgeranch31@gmail.com>
2023-12-20 21:12:43 +0000

    feat: update static campaign id

--- src/admin-portal/create-campaign/start-campaign.jsx
+++ src/admin-portal/create-campaign/start-campaign.jsx
@@ -50,7 +50,7 @@ export function StartCampaign ({ campaignDetails, setCampaignDetails, updateCamp
     try {
       setLoading(true);
       const payload = {
-        campaign_account_id: "24b4b3c0-5592-4548-b297-7fd57618a265", // TODO Remove this when the API is fixed
+        campaign_account_id: "c2699d84-92e1-436f-8aad-cd301dad97ec", // TODO Remove this when the API is fixed
         title: campaignDetails?.title,
         community_ids: campaignDetails?.communities?.map((community) => community?.id),
       }
