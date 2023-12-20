78465d6dc53ce3ae8ebeef15f3b4ff8e053f248d
Jephthah <boatengjephthah27@gmail.com>
2023-12-20 18:32:24 +0000

    managers page fix and campaign statistics data

--- src/api/urls.js
+++ src/api/urls.js
@@ -1,9 +1,9 @@
 import { IS_DEV, IS_PROD } from "./config";
 
-export var API_HOST = "https://api.massenergize.dev";
-// export var API_HOST = "http://localhost:8000/api";
+// export var API_HOST = "https://api.massenergize.dev";
+export var API_HOST = "http://localhost:8000/api";
 
 if (IS_PROD) API_HOST = "...";
-if (IS_DEV) API_HOST = "https://api.massenergize.dev";
+// if (IS_DEV) API_HOST = "https://api.massenergize.dev";
 
 export const CAMPAIGN_INFORMATION_URL = "campaigns.info";
