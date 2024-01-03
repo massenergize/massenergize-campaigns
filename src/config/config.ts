4b89bdecb400f8ba1f9e5fed8a785ea848f89fa0
Kobina_Koomson <georgeranch31@gmail.com>
2024-01-03 09:46:47 +0000

    refactor: externalize SWR config and move environment config to config directory

---
+++ src/config/config.ts
@@ -0,0 +1,8 @@
+export const SWR_CONFIG = {
+  dedupingInterval: 3_600_000,
+  revalidateInterval: 3_600_000,
+  refreshInterval: 3_600_000,
+  revalidateIfStale: false,
+  revalidateOnFocus: false,
+  revalidateOnReconnect: false,
+};
