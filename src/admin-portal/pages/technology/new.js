188639d9361e9e68e2c3c9cd83454915e437ed41
frimpongopoku <pongofrimi@gmail.com>
2024-01-04 14:39:46 +0000

    creation works

--- src/admin-portal/pages/technology/new.js
+++ src/admin-portal/pages/technology/new.js
@@ -5,136 +5,174 @@ import CreateTechnologyPageWrapper from "../PageWrapper/CreateTechnologyPageWrap
 import classes from "classnames";
 import { apiCall } from "../../../api/messenger";
 import { AdminLayout } from "../../../layouts/admin-layout";
+import { useBubblyBalloons } from "../../../lib/bubbly-balloon/use-bubbly-balloons";
+import { useParams } from "react-router-dom";
 
 const { useReducer } = require("react");
 
+const INFO_INITIAL_STATE = {
+  name: "",
+  image: "",
+  description: "",
+  summary: "",
+};
 const initialState = {
-	isTemplate: false,
-	title: "",
-	slogan: "",
-	startDate: "",
-	endDate: "",
-	description: "",
-	logo: "",
-	fullName: "",
-	email: "",
-	contact: "",
-	profileImage: "",
+  isTemplate: false,
+  title: "",
+  slogan: "",
+  startDate: "",
+  endDate: "",
+  description: "",
+  logo: "",
+  fullName: "",
+  email: "",
+  contact: "",
+  profileImage: "",
 };
 
 const reducer = (state, action) => {
-	switch (action.type) {
-		case "SET_FIELD_VALUE":
-			return { ...state, [action.field]: action.value };
-		default:
-			throw new Error(`Unsupported action type: ${action.type}`);
-	}
+  switch (action.type) {
+    case "SET_FIELD_VALUE":
+      return { ...state, [action.field]: action.value };
+    default:
+      throw new Error(`Unsupported action type: ${action.type}`);
+  }
 };
 
 export function CreateTechnology() {
-	const [showError, setShowError] = useState(false);
-	const [activeTab, setActiveTab] = useState(technologyPages[0].name);
+  const [showError, setShowError] = useState(false);
+  const [activeTab, setActiveTab] = useState(technologyPages[0].name);
 
-	const [technologyInfo, setTechnologyInfo] = useState({});
+  const [information, setInformation] = useState(INFO_INITIAL_STATE);
 
-	const [campaignDetails, dispatch] = useReducer(reducer, initialState);
+  const [campaignDetails, dispatch] = useReducer(reducer, initialState);
+  const { notify } = useBubblyBalloons();
 
-	const handleCampaignDetailsChange = (name, value) => {
-		dispatch({ type: "SET_FIELD_VALUE", field: name, value });
-	};
+  const { campaign_id, technology_id } = useParams();
 
-	const validateCampaignDetails = () => {
-		// FIXME change this to a more appropriate name
-		const {
-			title,
-			slogan,
-			startDate,
-			endDate,
-			description,
-			logo,
-			fullName,
-			email,
-			contact,
-			profileImage,
-		} = campaignDetails;
+  const notifyError = (message) => {
+    notify({
+      title: "Error",
+      message: message,
+      type: "error",
+      timeout: 15000,
+    });
+  };
+  const notifySuccess = (message) => {
+    notify({
+      title: "Error",
+      message: message,
+      type: "success",
+      timeout: 15000,
+    });
+  };
 
-		if (
-			!title ||
-			!slogan ||
-			!startDate ||
-			!endDate ||
-			!description ||
-			!logo ||
-			!fullName ||
-			!email ||
-			!contact ||
-			!profileImage
-		) {
-			setShowError(true);
-			return false;
-		}
+  //   const handleCampaignDetailsChange = (name, value) => {
+  //     dispatch({ type: "SET_FIELD_VALUE", field: name, value });
+  //   };
 
-		return true;
-	};
+  //   const validateCampaignDetails = () => {
+  //     // FIXME change this to a more appropriate name
+  //     const {
+  //       title,
+  //       slogan,
+  //       startDate,
+  //       endDate,
+  //       description,
+  //       logo,
+  //       fullName,
+  //       email,
+  //       contact,
+  //       profileImage,
+  //     } = campaignDetails;
 
-	const submitCampaign = () => {
-		try {
-			// TODO: validate campaign details
-			// TODO: submit campaign details
+  //     if (
+  //       !title ||
+  //       !slogan ||
+  //       !startDate ||
+  //       !endDate ||
+  //       !description ||
+  //       !logo ||
+  //       !fullName ||
+  //       !email ||
+  //       !contact ||
+  //       !profileImage
+  //     ) {
+  //       setShowError(true);
+  //       return false;
+  //     }
 
-			// apiCall("campaign/create", campaignDetails).then((res) => {
-			// 	console.log("==== res ====", res);
-			// });
-		} catch (e) {
-			throw Error("Error submitting campaign"); //FIXME chnage this to a more appropriate error message or even a mor detailed error object
-		}
-	};
+  //     return true;
+  //   };
 
-	return (
-		<div
-			style={{
-				padding: "1rem",
-			}}
-		>
-			<AdminLayout>
-					{/*region Header*/}
-					<Row
-						lg={{ gutter: 0 }}
-						className="pb-4 overflow-scroll gap-0 no-gutters g"
-					>
-						<Col>
-							<div className="nav-tabs-container">
-								{technologyPages?.map((page) => (
-									<div
-										key={page?.name}
-										className={classes("nav-tabs-main tab", {
-											"tab-active": activeTab === page?.name,
-										})}
-										onClick={() => setActiveTab(page?.name)}
-									>
-										<h5 className={classes("nav-tabs")}>{page?.name}</h5>
-									</div>
-								))}
-							</div>
-						</Col>
-					</Row>
-					{/*endregion*/}
+  //   const submitCampaign = () => {
+  //     try {
+  //       // TODO: validate campaign details
+  //       // TODO: submit campaign details
+  //       // apiCall("campaign/create", campaignDetails).then((res) => {
+  //       // 	console.log("==== res ====", res);
+  //       // });
+  //     } catch (e) {
+  //       throw Error("Error submitting campaign"); //FIXME chnage this to a more appropriate error message or even a mor detailed error object
+  //     }
+  //   };
 
-					{/*region Body: Content goes here*/}
-					<Row className="mt-4 pt-4">
-						<Col>
-							{technologyPages?.map((tab) => {
-								return (
-									activeTab === tab?.name && <tab.component key={tab?.name}  technologyInfo={technologyInfo} setTechnologyInfo ={setTechnologyInfo} setActiveTab={setActiveTab}/>
-								);
-							})}
-						</Col>
-					</Row>
-					{/*endregion*/}
+  return (
+    <div
+      style={{
+        padding: "1rem",
+      }}
+    >
+      <AdminLayout>
+        {/*region Header*/}
+        <Row
+          lg={{ gutter: 0 }}
+          className="pb-4 overflow-scroll gap-0 no-gutters g"
+        >
+          <Col>
+            <div className="nav-tabs-container">
+              {technologyPages?.map((page) => (
+                <div
+                  key={page?.name}
+                  className={classes("nav-tabs-main tab", {
+                    "tab-active": activeTab === page?.name,
+                  })}
+                  onClick={() => setActiveTab(page?.name)}
+                >
+                  <h5 className={classes("nav-tabs")}>{page?.name}</h5>
+                </div>
+              ))}
+            </div>
+          </Col>
+        </Row>
+        {/*endregion*/}
 
-					{/*region Footer*/}
-					{/*endregion*/}
-			</AdminLayout>
-		</div>
-	);
+        {/*region Body: Content goes here*/}
+        <Row className="mt-4 pt-4">
+          <Col>
+            {technologyPages?.map((tab) => {
+              return (
+                activeTab === tab?.name && (
+                  <tab.component
+                    key={tab?.name}
+                    setInformation={setInformation}
+                    information={information}
+                    notifyError={notifyError}
+                    notifySuccess={notifySuccess}
+                    // technologyInfo={technologyInfo}
+                    // setTechnologyInfo={setTechnologyInfo}
+                    setActiveTab={setActiveTab}
+                  />
+                )
+              );
+            })}
+          </Col>
+        </Row>
+        {/*endregion*/}
+
+        {/*region Footer*/}
+        {/*endregion*/}
+      </AdminLayout>
+    </div>
+  );
 }
--- src/components/admin-components/Button.jsx
+++ src/components/admin-components/Button.jsx
@@ -1,17 +1,23 @@
 import React, { useState } from "react";
 import "../../assets/styles/styles.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Spinner } from "react-bootstrap";
 
-const Button = ({ text, onSubmit, rounded, icon }) => {
-	// const [loader, setLoader] = useState(false);
-	return (
-		<button
-			onClick={onSubmit && onSubmit}
-			className={rounded ? `btn-rounded btn-n ` : `btn-fixed btn-n`}
-		>
-			<span> {icon && <FontAwesomeIcon icon={icon} />} </span> {text}
-		</button>
-	);
+const Button = ({ text, onSubmit, onClick, rounded, icon, loading = true }) => {
+  // const [loader, setLoader] = useState(false);
+  return (
+    <button
+      onClick={(e) => {
+        if (onClick) return onClick(e);
+        onSubmit && onSubmit(e);
+      }}
+      className={rounded ? `btn-rounded btn-n ` : `btn-fixed btn-n`}
+    >
+      {loading && <Spinner size="sm" style={{ marginRight: 6 }} />}
+      <span> {icon && <FontAwesomeIcon icon={icon} />} </span>{" "}
+      {!loading && <span>{text}</span>}
+    </button>
+  );
 };
 
 export default Button;
