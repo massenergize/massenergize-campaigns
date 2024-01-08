0a2343df1b53ee0a15b354c4488b28412e0c054f
Kobina_Koomson <georgeranch31@gmail.com>
2024-01-08 15:21:20 +0000

    feat: Add technology incentives handling functions
    The changes include adding two new methods to technology-requests.js file. One to handle updating technology incentives (updateTechnologyIncentives) and the other to handle adding technology incentives (addTechnologyIncentives). Also, indentation and line spacing are refined in the whole file for better readability.

--- src/requests/technology-requests.js
+++ src/requests/technology-requests.js
@@ -1,9 +1,9 @@
 import { apiCall } from "src/api/messenger";
 import { handleRequestError } from "src/helpers/utils/http";
 
-export const fetchEvents = async (community_ids,url = "events.list", ) => {
+export const fetchEvents = async (community_ids, url = "events.list",) => {
   try {
-    const response = await apiCall(url, { no_pagination : true, community_ids });
+    const response = await apiCall(url, { no_pagination: true, community_ids });
 
     if (!response || !response?.success) {
       handleRequestError(response?.error, "FETCH_EVENTS_ERROR_BE");
@@ -15,18 +15,44 @@ export const fetchEvents = async (community_ids,url = "events.list", ) => {
   }
 }
 
-
-
 export const addTechnologyEvent = async (data, url = "technologies.events.add") => {
-    try {
-        const response = await apiCall(url, data);
-    
-        if (!response || !response?.success) {
-        handleRequestError(response?.error, "ADD_EVENT_ERROR_BE");
-        }
-    
-        return response?.data;
-    } catch (error) {
-        handleRequestError(error, "ADD_EVENT_ERROR");
+  try {
+    const response = await apiCall(url, data);
+
+    if (!response || !response?.success) {
+      handleRequestError(response?.error, "ADD_EVENT_ERROR_BE");
     }
+
+    return response?.data;
+  } catch (error) {
+    handleRequestError(error, "ADD_EVENT_ERROR");
+  }
+}
+
+export const updateTechnologyIncentives = async (data, url = "technologies.overview.update") => {
+  try {
+    const response = await apiCall(url, data);
+
+    if (!response || !response?.success) {
+      handleRequestError(response?.error, "UPDATE_INCENTIVE_ERROR_BE");
+    }
+
+    return response?.data;
+  } catch (error) {
+    handleRequestError(error, "UPDATE_INCENTIVE_ERROR_BE");
+  }
+}
+
+export const addTechnologyIncentives = async (data, url = "technologies.overview.add") => {
+  try {
+    const response = await apiCall(url, data);
+
+    if (!response || !response?.success) {
+      handleRequestError(response?.error, "ADD_INCENTIVE_ERROR_BE");
+    }
+
+    return response?.data;
+  } catch (error) {
+    handleRequestError(error, "ADD_INCENTIVE_ERROR");
+  }
 }
