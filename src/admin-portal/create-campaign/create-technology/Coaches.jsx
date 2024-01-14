ee21fa3905239fc5d78cc8348eeb4f4e867bbd88
frimpongopoku <pongofrimi@gmail.com>
2024-01-14 06:13:30 +0000

    coaches page now uses a table view

--- src/admin-portal/create-campaign/create-technology/Coaches.jsx
+++ src/admin-portal/create-campaign/create-technology/Coaches.jsx
@@ -1,15 +1,18 @@
-import React, { useEffect, useReducer, useState } from "react";
+import React, { useEffect, useReducer, useRef, useState } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Input from "../../../components/admin-components/Input";
-import FileUploader, { RESET, } from "../../../components/admin-components/FileUploader";
+import FileUploader, {
+  RESET,
+} from "../../../components/admin-components/FileUploader";
 import Button from "../../../components/admin-components/Button";
 import "../../../assets/styles/admin-styles.scss";
 import Chip from "../../../components/admin-components/Chip";
 import { apiCall } from "src/api/messenger";
 import { MultiSelect } from "react-multi-select-component";
 import { useCampaignContext } from "../../../hooks/use-campaign-context";
+import BootButton from "react-bootstrap/Button";
 
 const INITIAL_COACH_STATE = {
   technology_id: "",
@@ -19,7 +22,7 @@ const INITIAL_COACH_STATE = {
   phone_number: "",
 };
 
-function Coaches ({
+function Coaches({
   setTechnologyInfo,
   technologyInfo,
   setCurrentTab,
@@ -30,19 +33,27 @@ function Coaches ({
   notifySuccess,
   tech_id,
 }) {
-  // const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
-  // const [selectedCoach, setSelectedCoach] = useState({});
-  // const [showCoachModal, setShowCoachModal] = useState(false);
   const [formData, setFormData] = useState(INITIAL_COACH_STATE);
-  // const [query, setQuery] = useState({});
   const [isEditing, setIsEditing] = useState(false);
+  const [showCoachForm, setShowCoachForm] = useState(false);
+  const coachFormRef = useRef(null);
 
-  // TODO LATER: EDITING & CREATION CAN BE MERGED INTO ONE FORM AND ONE PROCESS!
-  // const buildQuery = (key, data) => {
+  const scrollToSection = (ref) => {
+    console.log("LE REF U SAY", ref);
+    if (ref.current)
+      ref.current.scrollIntoView({ behavior: "smooth", block: "start" });
+  };
 
-  //   setQuery({ ...query, [key]: data });
-  // };
+  // useEffect(() => {
+  //   scrollToSection(coachFormRef);
+  // }, [coachFormRef]);
+
+  const interactWithCoachForm = () => {
+    setShowCoachForm(true);
+    if (coachFormRef?.current)
+      coachFormRef.current.scrollIntoView({ behavior: "smooth", block: "start" });
+  };
 
   const getValue = (key, source) => {
     source = source || formData;
@@ -50,7 +61,6 @@ function Coaches ({
   };
 
   const handleFieldChange = (field, value) => {
-    // dispatch({ type: "SET_FIELD_VALUE", field, value });
     setFormData({ ...formData, [field]: value });
   };
 
@@ -58,7 +68,7 @@ function Coaches ({
     const { full_name, image, email, community } = data || {};
     if (!full_name || !image || !email || !community) {
       notifyError(
-        "(Email, Full Name, Image, Community) Please make sure none of them are empty..."
+        "(Email, Full Name, Image, Community) Please make sure none of them are empty...",
       );
       return false;
     }
@@ -68,7 +78,6 @@ function Coaches ({
   const handleSubmit = async (e) => {
     e.preventDefault();
     let data = { ...formData, technology_id: tech_id };
-    // return console.log("THis is what the data looks like", data);
     const url = isEditing
       ? "technologies.coaches.update"
       : "technologies.coaches.create";
@@ -110,132 +119,206 @@ function Coaches ({
           <p className="my-4">
             All the coaches you add will appear here. Add as many as you need!
           </p>
-          {coaches?.map((item, index) => {
-            return (
-              <Chip
-                // className="mr-5"
-                style={{ marginRight: 6 }}
-                key={index}
-                text={item?.full_name}
-                onDismiss={() => {
-                  let items = [...coaches];
-                  items.splice(index, 1);
-                  setCoaches(items);
-                }}
-                onClick={() => {
-                  const editObj = { ...item, image: item?.image?.url || "" };
-                  setFormData(editObj);
-                  setIsEditing(true);
-                  // setQuery();
-                  // setShowCoachModal(true);
-                }}
-              />
-            );
-          })}
+          <div style={{ display: "flex", flexDirection: "column", width: "100%" }}>
+            {coaches?.map((item, index) => {
+              const { full_name, id, image } = item;
+              return (
+                <div
+                  key={id?.toString()}
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    border: "solid 0px var(--bs-border-color)",
+                    borderBottomWidth: 1,
+                    padding: "15px 0px",
+                  }}
+                >
+                  <img
+                    alt={full_name + " image"}
+                    src={image?.url}
+                    style={{
+                      width: 40,
+                      height: 40,
+                      marginRight: 20,
+                      objectFit: "cover",
+                      borderRadius: "100%",
+                    }}
+                  />
+                  <h6 style={{ margin: 0 }}>{full_name}</h6>{" "}
+                  <div style={{ marginLeft: "auto" }}>
+                    <BootButton
+                      onClick={() => {
+                        const editObj = { ...item, image: item?.image?.url || "" };
+                        setFormData(editObj);
+                        setIsEditing(true);
+                        // setQuery();
+                        // setShowCoachModal(true);
+                      }}
+                    >
+                      Edit
+                    </BootButton>
+                    <BootButton variant="danger" style={{ margin: "0px 5px" }}>
+                      Remove
+                    </BootButton>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+
+          <h6
+            role="button"
+            className="touchable-opacity"
+            style={{
+              color: "var(--admin-theme-color)",
+              padding: "20px 0px",
+              display: "inline-block",
+            }}
+            onClick={() => interactWithCoachForm()}
+          >
+            <i className="fa fa-plus" style={{ marginRight: 6 }}></i> Add a new coach{" "}
+          </h6>
         </Col>
-        {coaches?.length ? (
-          <small style={{ color: "grey", marginTop: 8 }}>
-            Click on any coach to edit
-          </small>
-        ) : (
-          <></>
-        )}
       </Row>
     );
+    // return (
+    //   <Row className="py-4">
+    //     <Col>
+    //       <h5 className="theme-color">Selected Coaches</h5>
+    //       <p className="my-4">
+    //         All the coaches you add will appear here. Add as many as you need!
+    //       </p>
+    //       {coaches?.map((item, index) => {
+    //         return (
+    //           <Chip
+    //             // className="mr-5"
+    //             style={{ marginRight: 6 }}
+    //             key={index}
+    //             text={item?.full_name}
+    //             onDismiss={() => {
+    //               let items = [...coaches];
+    //               items.splice(index, 1);
+    //               setCoaches(items);
+    //             }}
+    //             onClick={() => {
+    //               const editObj = { ...item, image: item?.image?.url || "" };
+    //               setFormData(editObj);
+    //               setIsEditing(true);
+    //               // setQuery();
+    //               // setShowCoachModal(true);
+    //             }}
+    //           />
+    //         );
+    //       })}
+    //     </Col>
+    //     {coaches?.length ? (
+    //       <small style={{ color: "grey", marginTop: 8 }}>
+    //         Click on any coach to edit
+    //       </small>
+    //     ) : (
+    //       <></>
+    //     )}
+    //   </Row>
+    // );
   };
 
-  const {
-    campaignDetails,
-    originalCampaignDetails,
-    lists,
-    handleCampaignDetailsChange : setCampaignDetails,
-    setNewCampaignDetails,
-  } = useCampaignContext();
+  const { lists, handleCampaignDetailsChange: setCampaignDetails } =
+    useCampaignContext();
 
-  const {
-    allCommunities,
-  } = lists;
+  const { allCommunities } = lists;
 
   const formHasContent = Object.keys(formData || {}).length > 0;
   return (
     <div>
       <Container>
+        <div style={{ display: "flex", flexDirection: "row", width: "100%" }}>
+          <BootButton
+            style={{ marginLeft: "auto" }}
+            variant="success"
+            onClick={() => interactWithCoachForm()}
+          >
+            <i className="fa fa-plus" style={{ marginRight: 7 }} />
+            Add A New Coach
+          </BootButton>
+        </div>
         {coaches?.length > 0 && (
           <Container>
             {/* <hr /> */}
             {renderSelectedCoaches()}
           </Container>
         )}
-        <form style={{ border: "dashed 2px #eeeeee", padding: "40px" }}>
-          <Row>
-            <Col>
-              <h5 className="theme-color">
-                {isEditing
-                  ? `Editing "${formData?.full_name}"`
-                  : "Add A New Coach"}
-              </h5>
-              <p className="my-4">
-                Please include details of the new Coach of this technology
-              </p>
-            </Col>
-          </Row>
-          <Row className="py-2">
-            <Col>
-              <Input
-                label="Full Name"
-                placeholder="Enter full name here..."
-                required={true}
-                type="textbox"
-                value={getValue("full_name")}
-                onChange={(val) => {
-                  handleFieldChange("full_name", val);
-                }}
-              />
-            </Col>
-          </Row>
-          <Row className="py-2">
-            <Col>
-              <Input
-                label="Email"
-                placeholder="Enter email here..."
-                required={true}
-                type="email"
-                value={getValue("email")}
-                onChange={(val) => {
-                  handleFieldChange("email", val);
-                }}
-              />
-            </Col>
-          </Row>
-          <Row className="py-2">
-            <Col>
-              <Input
-                label="Phone Number"
-                placeholder="Enter email here..."
-                required={false}
-                type="textbox"
-                value={getValue("phone_number")}
-                onChange={(val) => {
-                  handleFieldChange("phone_number", val);
-                }}
-              />
-            </Col>
-          </Row>
-          <Row className="py-2">
-            <Col>
-              <Input
-                label="Community"
-                placeholder="Enter the community of the coach here..."
-                required={true}
-                type="textbox"
-                value={getValue("community")}
-                onChange={(val) => {
-                  handleFieldChange("community", val);
-                }}
-              />
-            </Col>
-          </Row>
-          {/*<Row className="py-2">
+        <div ref={coachFormRef}>
+          {showCoachForm && (
+            <form style={{ border: "dashed 2px #eeeeee", padding: "40px" }}>
+              <Row>
+                <Col>
+                  <h5 className="theme-color">
+                    {isEditing
+                      ? `Editing "${formData?.full_name}"`
+                      : "Add A New Coach"}
+                  </h5>
+                  <p className="my-4">
+                    Please include details of the new Coach of this technology
+                  </p>
+                </Col>
+              </Row>
+              <Row className="py-2">
+                <Col>
+                  <Input
+                    label="Full Name"
+                    placeholder="Enter full name here..."
+                    required={true}
+                    type="textbox"
+                    value={getValue("full_name")}
+                    onChange={(val) => {
+                      handleFieldChange("full_name", val);
+                    }}
+                  />
+                </Col>
+              </Row>
+              <Row className="py-2">
+                <Col>
+                  <Input
+                    label="Email"
+                    placeholder="Enter email here..."
+                    required={true}
+                    type="email"
+                    value={getValue("email")}
+                    onChange={(val) => {
+                      handleFieldChange("email", val);
+                    }}
+                  />
+                </Col>
+              </Row>
+              <Row className="py-2">
+                <Col>
+                  <Input
+                    label="Phone Number"
+                    placeholder="Enter email here..."
+                    required={false}
+                    type="textbox"
+                    value={getValue("phone_number")}
+                    onChange={(val) => {
+                      handleFieldChange("phone_number", val);
+                    }}
+                  />
+                </Col>
+              </Row>
+              <Row className="py-2">
+                <Col>
+                  <Input
+                    label="Community"
+                    placeholder="Enter the community of the coach here..."
+                    required={true}
+                    type="textbox"
+                    value={getValue("community")}
+                    onChange={(val) => {
+                      handleFieldChange("community", val);
+                    }}
+                  />
+                </Col>
+              </Row>
+              {/*<Row className="py-2">
             <Col>
               <MultiSelect
                 options={(allCommunities?.data || []).map((campaign) => {
@@ -254,55 +337,56 @@ function Coaches ({
 
             </Col>
           </Row>*/}
-          <Row className="py-4">
-            <Col>
-              <FileUploader
-                required={false}
-                id="coach_image"
-                text="Add a profile image for the Coach"
-                onChange={(val) => {
-                  handleFieldChange("image", val);
-                }}
-                // value={getValue("image")}
-                defaultValue={getValue("image")}
-              />
-            </Col>
-          </Row>
+              <Row className="py-4">
+                <Col>
+                  <FileUploader
+                    required={false}
+                    id="coach_image"
+                    text="Add a profile image for the Coach"
+                    onChange={(val) => {
+                      handleFieldChange("image", val);
+                    }}
+                    // value={getValue("image")}
+                    defaultValue={getValue("image")}
+                  />
+                </Col>
+              </Row>
 
-          <Row className="py-4 mt-4 justify-content-end">
-            <Col
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                alignItems: "center",
-                // justifyContent: "center",
-              }}
-            >
-              <Button
-                loading={loading}
-                text={isEditing ? "Save Changes" : "Add Coach"}
-                onSubmit={handleSubmit}
-                rounded={false}
-              />
-              {formHasContent && (
-                <small
-                  onClick={() => resetForm()}
+              <Row className="py-4 mt-4 justify-content-end">
+                <Col
                   style={{
-                    fontWeight: "bold",
-                    marginLeft: 10,
-                    color: "red",
-                    textDecoration: "underline",
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "center",
+                    // justifyContent: "center",
                   }}
-                  className="touchable-opacity"
                 >
-                  Reset Form
-                </small>
-              )}
-            </Col>
-          </Row>
-        </form>
+                  <Button
+                    loading={loading}
+                    text={isEditing ? "Save Changes" : "Add Coach"}
+                    onSubmit={handleSubmit}
+                    rounded={false}
+                  />
+                  {formHasContent && (
+                    <small
+                      onClick={() => resetForm()}
+                      style={{
+                        fontWeight: "bold",
+                        marginLeft: 10,
+                        color: "red",
+                        textDecoration: "underline",
+                      }}
+                      className="touchable-opacity"
+                    >
+                      Reset Form
+                    </small>
+                  )}
+                </Col>
+              </Row>
+            </form>
+          )}
+        </div>
       </Container>
-
     </div>
   );
 
@@ -382,6 +466,6 @@ function Coaches ({
   //     </div>
   //   );
   // }
-};
+}
 
 export default Coaches;
