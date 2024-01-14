611c9a8b84ebf3010a04be0d0f64663d627ba659
Jephthah <boatengjephthah27@gmail.com>
2024-01-14 09:24:01 +0000

    vendors showing associated community

--- src/admin-portal/create-campaign/create-technology/Vendors.jsx
+++ src/admin-portal/create-campaign/create-technology/Vendors.jsx
@@ -6,25 +6,32 @@ import Chip from "src/components/admin-components/Chip";
 import { useBubblyBalloons } from "src/lib/bubbly-balloon/use-bubbly-balloons";
 import Button from "../../../components/admin-components/Button";
 import useSWR from "swr";
-import { addTechnologyVendor, fetchCampaignCommunityVendors } from "src/requests/technology-requests";
+import {
+  addTechnologyVendor,
+  fetchCampaignCommunityVendors,
+} from "src/requests/technology-requests";
 
-const Vendors = ({campaign_id, tech_id,techObject, updateTechObject}) => {
+const Vendors = ({ campaign_id, tech_id, techObject, updateTechObject }) => {
+  let existing = [
+    ...(techObject?.vendors || [])?.map((tech) => tech?.vendor),
+  ].flat();
+  const [selectedVendors, setSelectedVendors] = useState(existing || []);
+  const [loading, setLoading] = useState(false);
 
-	let existing = [...(techObject?.vendors||[])?.map((tech) => tech?.vendor)].flat();
-	const [selectedVendors, setSelectedVendors] = useState(existing || []);
-	const [loading, setLoading] = useState(false);
+  const { blow, pop } = useBubblyBalloons();
 
-	const { blow, pop } = useBubblyBalloons();
+  const { data: allVendors, isLoading } = useSWR(
+    "campaigns.communities.vendors.list",
+    () => fetchCampaignCommunityVendors(campaign_id),
+    {
+      shouldRetryOnError: true,
+      errorRetryCount: 3,
+      errorRetryInterval: 3000,
+    },
+  );
 
-	const {data:allVendors, isLoading} = useSWR("campaigns.communities.vendors.list", () => fetchCampaignCommunityVendors(campaign_id), {
-		shouldRetryOnError: true,
-		errorRetryCount: 3,
-		errorRetryInterval: 3000,
-	});
-
-
-	const handleSaveVendors = async () => {
-	    setLoading(true);
+  const handleSaveVendors = async () => {
+    setLoading(true);
     try {
       let toSend = {
         technology_id: tech_id,
@@ -51,10 +58,9 @@ const Vendors = ({campaign_id, tech_id,techObject, updateTechObject}) => {
         duration: 5000,
       });
     }
+  };
 
-	}
-
-	if (isLoading)
+  if (isLoading)
     return (
       <div
         className=""
@@ -69,82 +75,85 @@ const Vendors = ({campaign_id, tech_id,techObject, updateTechObject}) => {
       </div>
     );
 
-	return (
-		<div style={{ height: "100vh" }}>
-		<Container>
-		  <Form>
-			<FormLabel>
-			  Choose one or more Vendors for this technology from the dropdown
-			  below.
-			</FormLabel>
-  
-			<MultiSelect
-			  options={(allVendors || []).map((vendor) => {
-				return {
-				  ...vendor,
-				  value: vendor?.id,
-				  label: vendor?.name,
-				};
-			  })}
-			  hasSelectAll={true}
-			  value={selectedVendors?.map((vendor) => {
-				return {
-				  ...vendor,
-				  value: vendor?.id,
-				  label: vendor?.name,
-			  }})}
-			  onChange={(val) => setSelectedVendors(val)}
-			  valueRenderer={(selected, _options) => {
-				if(selected.length === 0) return "Select Vendors"
-				if (selected.length === _options.length) return "All Vendors Selected";
-				if (selected.length > 2) return `${selected.length} Vendors Selected`;
-				return selected?.map(({ label }) => label).join(", ").concat(" Selected");
-			  }}
-			  className={"event-select"}
-			/>
-		  </Form>
-  
-		  <Row className="mt-4">
-			<Col>
-			  <Row>
-				{selectedVendors?.map((vendor) => {
-				  return (
-					<Col sm={"auto mb-2"}>
-					  <Chip
-						text={vendor?.name}
-						icon={vendor?.icon}
-						id={vendor?.id}
-						size={"sm"}
-						className="mr-2 mb-5"
-						onDismiss={(id, text) => {
-						  setSelectedVendors(
-							selectedVendors?.filter(
-							  (vendor) => vendor?.id !== id
-							)
-						  );
-						}}
-					  />
-					</Col>
-				  );
-				})}
-			  </Row>
-			</Col>
-		  </Row>
-  
-		  <Row className="mt-4 justify-content-end">
-			<Col>
-			  <Button
-				text="Save Changes"
-				loading={loading}
-				disabled={loading}
-				onSubmit={handleSaveVendors}
-				rounded={false}
-			  />
-			</Col>
-		  </Row>
-		</Container>
-	  </div>
-	);
+  console.log(allVendors);
+
+  return (
+    <div style={{ height: "100vh" }}>
+      <Container>
+        <Form>
+          <FormLabel>
+            Choose one or more Vendors for this technology from the dropdown below.
+          </FormLabel>
+
+          <MultiSelect
+            options={(allVendors || []).map((vendor) => {
+              return {
+                ...vendor,
+                value: vendor?.id,
+                label: `${vendor?.name} - ${vendor?.communities[0]?.name} Community`,
+              };
+            })}
+            hasSelectAll={true}
+            value={selectedVendors?.map((vendor) => {
+              return {
+                ...vendor,
+                value: vendor?.id,
+                label: vendor?.name,
+              };
+            })}
+            onChange={(val) => setSelectedVendors(val)}
+            valueRenderer={(selected, _options) => {
+              if (selected.length === 0) return "Select Vendors";
+              if (selected.length === _options.length) return "All Vendors Selected";
+              if (selected.length > 2) return `${selected.length} Vendors Selected`;
+              return selected
+                ?.map(({ label }) => label)
+                .join(", ")
+                .concat(" Selected");
+            }}
+            className={"event-select"}
+          />
+        </Form>
+
+        <Row className="mt-4">
+          <Col>
+            <Row>
+              {selectedVendors?.map((vendor) => {
+                return (
+                  <Col sm={"auto mb-2"}>
+                    <Chip
+                      text={vendor?.name}
+                      icon={vendor?.icon}
+                      id={vendor?.id}
+                      size={"sm"}
+                      className="mr-2 mb-5"
+                      onDismiss={(id, text) => {
+                        setSelectedVendors(
+                          selectedVendors?.filter((vendor) => vendor?.id !== id),
+                        );
+                      }}
+                    />
+                  </Col>
+                );
+              })}
+            </Row>
+          </Col>
+        </Row>
+
+        <Row className="mt-4 justify-content-end">
+          <Col>
+            <Button
+              text="Save Changes"
+              loading={loading}
+              disabled={loading}
+              onSubmit={handleSaveVendors}
+              rounded={false}
+            />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default Vendors;
