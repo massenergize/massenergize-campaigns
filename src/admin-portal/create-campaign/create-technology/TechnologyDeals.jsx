ab4ced14a2be1ab1e716a0c56522f0794008d3d0
Opoku-Agyemang <samuel.opokuagyemang@gmail.com>
2024-01-14 22:55:25 +0000

    space before bracket

--- src/admin-portal/create-campaign/create-technology/TechnologyDeals.jsx
+++ src/admin-portal/create-campaign/create-technology/TechnologyDeals.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function TechnologyDeals({ campaign_id, tech_id, techObject, updateTechObject }) {
+export default function TechnologyDeals ({ campaign_id, tech_id, techObject, updateTechObject }) {
   return (
     <div>
 
