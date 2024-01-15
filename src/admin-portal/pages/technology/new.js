903e8d518d43541f7c2b4909d6128cab6181fc52
Kobina_Koomson <georgeranch31@gmail.com>
2024-01-15 03:53:01 +0000

    chore: Clean up code and adjust padding in CreateTechnology function
    This commit removes extra spaces and improves layout in CreateTechnology function found in 'new.js' file of technology page in the admin portal. This mini refactor enhances the readability of the code and aligns the function's aesthetics with design specifications.

--- src/admin-portal/pages/technology/new.js
+++ src/admin-portal/pages/technology/new.js
@@ -3,8 +3,6 @@ import { AdminLayout } from "../../../layouts/admin-layout";
 import { TechnologyEditView } from "../../../views/technology-edit-view";
 
 export function CreateTechnology () {
-
-
   return (
     <AdminLayout>
       <TechnologyEditView />
