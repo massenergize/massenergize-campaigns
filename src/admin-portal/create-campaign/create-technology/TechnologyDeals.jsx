7ec0a8ed0999d6913c4845a0af14941290199c0b
Kobina_Koomson <georgeranch31@gmail.com>
2024-01-22 13:02:15 +0000

    refactor: Simplify TechnologyDeals.jsx structure and UI
    Redesigned the UI by removing unnecessary text and adjusting the placement of the 'Add New Deal' button in the TechnologyDeals component. This change makes the interface more clean and user-friendly.

--- src/admin-portal/create-campaign/create-technology/TechnologyDeals.jsx
+++ src/admin-portal/create-campaign/create-technology/TechnologyDeals.jsx
@@ -73,10 +73,7 @@ export default function TechnologyDeals({ campaign_id, tech_id, techObject, upda
             onClick={() => setOpenAccordion(!openAccordion)}
           />
         </div>
-      <Row className="py-3">
-        <Col className="">
-          <p>What are the deals for this campaign</p>
-        </Col>
+      <Row className="py-3 justify-content-end">
         <Col sm={"auto"}>
           <Button text="" rounded onClick={() => setOpenDealsModal(true)}>
             Add New Deal
