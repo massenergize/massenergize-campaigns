12cec01e94b113bd554349c9d64f852a57321ae1
frimpongopoku <pongofrimi@gmail.com>
2024-01-23 12:10:51 +0000

    small small login tweaks bi

--- src/admin-portal/pages/auth/Login.js
+++ src/admin-portal/pages/auth/Login.js
@@ -1,18 +1,25 @@
 import React, { useState } from "react";
-import { Button, Container, Form, FormGroup, FormLabel, Spinner, } from "react-bootstrap";
+import { Button, Container, Form, FormGroup, FormLabel, Spinner } from "react-bootstrap";
 import { validateEmail } from "../../../utils/utils";
 import Notification from "../../../components/pieces/Notification";
 import { signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
 import { auth, googleProvider } from "../../../config/firebase/admin/fire-config";
 import { connect, useDispatch } from "react-redux";
 import { bindActionCreators } from "redux";
-import { fetchMeUser, logUserOut, setAuthAdminAction, setCampaignAccountAction, setFirebaseAuthAction, } from "../../../redux/actions/actions";
+import {
+  fetchMeUser,
+  logUserOut,
+  setAuthAdminAction,
+  setCampaignAccountAction,
+  setFirebaseAuthAction,
+} from "../../../redux/actions/actions";
 import { useNavigate } from "react-router-dom";
+import { PLATFORM } from "../../../api/urls";
 
 const GOOGLE = "GOOGLE";
 const EMAIL = "EMAIL";
 
-function Login ({ logUserOut, fetchMassenergizeUser, putFirebaseAuthInRedux, }) {
+function Login({ logUserOut, fetchMassenergizeUser, putFirebaseAuthInRedux }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
@@ -38,17 +45,17 @@ function Login ({ logUserOut, fetchMassenergizeUser, putFirebaseAuthInRedux, }) 
 
         putFirebaseAuthInRedux(user);
         fetchMassenergizeUser({ idToken: user?.accessToken }, (data, err) => {
-            if (err) {
-                return setError(err);
-            }
-            if (data?.campaign_accounts?.length < 1) {
-              return navigate("/admin/campaign/account/new");
-            }else{
-              let encoded = btoa(JSON.stringify(data?.campaign_accounts[0]));
-              localStorage.setItem("acc", encoded);
-              dispatch(setCampaignAccountAction(data?.campaign_accounts[0]));
-              return navigate("/admin/home");
-            }
+          if (err) {
+            return setError(err);
+          }
+          if (data?.campaign_accounts?.length < 1) {
+            return navigate("/admin/campaign/account/new");
+          } else {
+            let encoded = btoa(JSON.stringify(data?.campaign_accounts[0]));
+            localStorage.setItem("acc", encoded);
+            dispatch(setCampaignAccountAction(data?.campaign_accounts[0]));
+            return navigate("/admin/home");
+          }
         });
       })
       .catch((e) => {
@@ -62,8 +69,7 @@ function Login ({ logUserOut, fetchMassenergizeUser, putFirebaseAuthInRedux, }) 
   const submit = () => {
     setAuthType(EMAIL);
     const emailIsValid = validateEmail(email?.trim());
-    if (!emailIsValid)
-      return setError("Please include a valid email and password");
+    if (!emailIsValid) return setError("Please include a valid email and password");
     setError("");
     setLoading(true);
 
@@ -89,50 +95,42 @@ function Login ({ logUserOut, fetchMassenergizeUser, putFirebaseAuthInRedux, }) 
   return (
     <Container
       className="d-flex align-items-center justify-content-center"
-      style={{ height: "100vh", padding: 0 }}
+      style={{
+        flexDirection: "column",
+        height: "100vh",
+        padding: 0,
+        maxWidth: "100vw",
+        background: "linear-gradient(357deg, rgba(110,32,124,1) 7%, rgba(255,255,255,1) 57%)",
+      }}
     >
+      <div style={{ padding: "50px 0px", marginTop: "-9%", textAlign: "center", color: "#b322cd" }}>
+        <h1 style={{ fontWeight: "900" }}>Campaign Admin Portal</h1>
+        <p>
+          <span style={{ textDecoration: "underline" }}> Sign in to manage your campaigns</span> <br />
+          <span style={{ color: "black" }}>
+            You need to be an existing massenergize admin before you can use this site
+          </span>
+        </p>
+      </div>
+
       <div
         className="elevate-float-pro"
         style={{
           minHeight: "29vh",
           width: "65vh",
           background: "white",
-          borderRadius: 6,
+          borderRadius: 10,
         }}
       >
         <div style={{ padding: 30 }}>
-          <div style={{ textAlign: "center" }} className={"mb-4 border-bottom pb-4"}>
+          {/* <div style={{ textAlign: "center" }} className={""}>
             <h3 style={{ color: "var(--admin-theme-color)" }}>Login</h3>
             <small className={"text-center"}>Sign in to manage your campaigns</small>
-            {/*<small>Sign in as a campaign administrator to manage your campaigns</small>*/}
-
-            <div>
-              <Button
-                onClick={() => authenticateWithGoogle()}
-                className="touchable-opacity px-4 py-2 mt-3 block"
-                disabled={loading}
-                variant={"dark"}
-                style={{
-                  marginRight: 10,
-                  // background: "#ee0c0c",
-                  fontWeight: "bold",
-                  borderWidth: 0,
-                }}
-              >
-                {loading && isGoogleAuth && (
-                  <Spinner size="sm" style={{ marginRight: 5 }}></Spinner>
-                )}
-                <span>
-                <img src="/img/google.svg" alt="Google Logo" className={"mr-3"}/> Login With Google </span>
-              </Button>
-            </div>
-          </div>
-          <div style={{ textAlign: "center" }} className={"mb-4"}>
-
-          </div>
+          </div> */}
+          {/* <div style={{ textAlign: "center" }} className={"mb-4"}></div> */}
           <div style={{ padding: "10px 20px" }}>
             <FormGroup className="mb-3">
-              <FormLabel style={{ marginLeft: 5 }}>Email</FormLabel>
+              <FormLabel style={{ marginLeft: 5, fontWeight: "bold" }}>Enter your email</FormLabel>
               <Form.Control
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
@@ -143,7 +141,7 @@ function Login ({ logUserOut, fetchMassenergizeUser, putFirebaseAuthInRedux, }) 
               />
             </FormGroup>
             <FormGroup className="mb-3">
-              <FormLabel style={{ marginLeft: 5 }}>Password</FormLabel>
+              <FormLabel style={{ marginLeft: 5, fontWeight: "bold" }}>Enter your password</FormLabel>
               <Form.Control
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
@@ -156,20 +154,40 @@ function Login ({ logUserOut, fetchMassenergizeUser, putFirebaseAuthInRedux, }) 
             <Notification show={error}>{error}</Notification>
           </div>
 
-          <div style={{ display: "flex", flexDirection: "row", padding: "0px 20px", }}>
+          <div style={{ display: "flex", flexDirection: "row", padding: "0px 20px", alignItems: "center" }}>
+            <small style={{ fontWeight: "bold", fontSize: 12, color: "rgb(227 227 227)" }}>{PLATFORM}</small>
             {/* <Button onClick={() => logUserOut()}>Sign Out</Button> */}
             <div style={{ display: "inline-block", marginLeft: "auto" }}>
+              {/* <div> */}
+              <Button
+                onClick={() => authenticateWithGoogle()}
+                className="touchable-opacity px-4 py-2  block"
+                disabled={loading}
+                variant={"dark"}
+                style={{
+                  marginRight: 10,
+                  // background: "#ee0c0c",
+                  fontWeight: "bold",
+                  borderWidth: 0,
+                }}
+              >
+                {loading && isGoogleAuth && <Spinner size="sm" style={{ marginRight: 5 }}></Spinner>}
+                <span>
+                  <img src="/img/google.svg" alt="Google Logo" className={"mr-3"} /> Login With Google{" "}
+                </span>
+              </Button>
+              {/* </div> */}
               <Button
                 onClick={() => submit()}
                 disabled={loading || !email || !password}
                 className="touchable-opacity"
                 style={{
-                  fontWeight: "bold", background: "var(--admin-theme-color)", borderWidth: 0,
+                  fontWeight: "bold",
+                  background: "var(--admin-theme-color)",
+                  borderWidth: 0,
                 }}
               >
-                {loading && isEmailAndPass && (
-                  <Spinner size="sm" style={{ marginRight: 5 }}></Spinner>
-                )}
+                {loading && isEmailAndPass && <Spinner size="sm" style={{ marginRight: 5 }}></Spinner>}
                 <span>Submit</span>
               </Button>
             </div>
@@ -188,6 +206,10 @@ function Login ({ logUserOut, fetchMassenergizeUser, putFirebaseAuthInRedux, }) 
           <Button style={{ marginLeft: "auto" }}>Here we go</Button>
         </div> */}
       </div>
+
+      {/* <div style={{ padding: 20, color: "#6e207c4a" }}>
+        <h6>Development</h6>
+      </div> */}
 
       {/* <Row>
         <Col className="mx-auto">
@@ -222,13 +244,14 @@ function Login ({ logUserOut, fetchMassenergizeUser, putFirebaseAuthInRedux, }) 
 // }
 
 const mapDispatch = (dispatch) => {
-  return bindActionCreators({
+  return bindActionCreators(
+    {
       logUserOut,
       putAdminInRedux: setAuthAdminAction,
       fetchMassenergizeUser: fetchMeUser,
       putFirebaseAuthInRedux: setFirebaseAuthAction,
     },
-    dispatch
+    dispatch,
   );
 };
 export default connect(null, mapDispatch)(Login);
