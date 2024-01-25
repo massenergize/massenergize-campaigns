07c6024b40ba125540b7836235e1d05ead0a6973
Kobina_Koomson <georgeranch31@gmail.com>
2024-01-25 16:10:03 +0000

    feat: Update default API host URL and add DialogProvider
    Changed the default API host URL in `urls.js` from `localhost` to `https://api.massenergize.dev` for a more consistent development environment. Also, wrapped `BrowserRouter` with `DialogProvider` in `index.js` to allow usage of dialog context throughout the application.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
@@ -11,18 +10,21 @@ import AppRouter from "./routes/AppRouter";
 import "@kehillahglobal/ui/dist/index.css";
 import BubblyBalloonProvider from "./lib/bubbly-balloon/bubbly-balloon-context";
 import ErrorBoundary from "./components/ErrorBoundary";
+import { DialogProvider } from "./lib/dicey-dialog/dicey-dialog-context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ErrorBoundary>
     <Provider store={store}>
       <BubblyBalloonProvider>
-        <BrowserRouter>
-          <AppRouter />
-        </BrowserRouter>
+        <DialogProvider>
+          <BrowserRouter>
+            <AppRouter />
+          </BrowserRouter>
+        </DialogProvider>
       </BubblyBalloonProvider>
     </Provider>
-  </ErrorBoundary>
+  </ErrorBoundary>,
 );
 
 // If you want to start measuring performance in your app, pass a function
