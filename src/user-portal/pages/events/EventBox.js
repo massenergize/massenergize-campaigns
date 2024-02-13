97d8b046b62658470f1b50470e4a6396921fcd85
Kobina_Koomson <georgeranch31@gmail.com>
2024-02-13 09:28:48 +0000

    fix: Refactor EventBox presentation layout
    Redesigned EventBox's layout using bootstrap Card with a more structured and modular approach. Removed unused formatTimeRange import from utils, fixed potential layout issues with cards, and enhanced overall user interface for a better visual experience.

--- src/user-portal/pages/events/EventBox.js
+++ src/user-portal/pages/events/EventBox.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { formatDate, formatTime, formatTimeRange, smartString } from "../../../utils/utils";
+import { formatDate, formatTime, smartString } from "../../../utils/utils";
 import { Col, Row } from "react-bootstrap";
 
 function EventBox({ event, campaign_technology }) {
@@ -9,31 +9,41 @@ function EventBox({ event, campaign_technology }) {
   const { name, image, start_date, end_date, id } = event || {};
   const navigator = useNavigate();
 
-  function gotoEvent () {
-    navigator(`/campaign/${campaign?.slug}/technology/event/${id}`)
+  function gotoEvent() {
+    navigator(`/campaign/${campaign?.slug}/technology/event/${id}`);
   }
 
   return (
-    <div className="border rounded-4 p-3 bg-white mb-3 h-100">
-      {image?.url && (
-        <img
-          className={"rounded-3 w-100 cursor-pointer"}
-          style={{ height: 180, objectFit: "cover", borderRadius: 5 }}
-          src={image?.url}
-          alt={"event"}
-          role={"button"} tabIndex={0}
-          onClick={gotoEvent}
-        />
-      )}
-      <div>
-        <h6 className="touchable-opacity body-font mt-2 mb-1" role={"button"} tabIndex={0} onClick={() => gotoEvent()}>
-          {smartString(name, 50) || "..."}
-        </h6>
-
+    <div className="card border rounded-4 p-3 bg-white mb-3 h-100 d-flex flex-col justify-content-between">
+      <div className="card-body p-0">
+        {image?.url && (
+          <img
+            className={"rounded-3 w-100 cursor-pointer"}
+            style={{ height: 180, objectFit: "cover", borderRadius: 5 }}
+            src={image?.url}
+            alt={"event"}
+            role={"button"}
+            tabIndex={0}
+            onClick={gotoEvent}
+          />
+        )}
+        <div>
+          <h6
+            className="touchable-opacity body-font mt-2 mb-1"
+            role={"button"}
+            tabIndex={0}
+            onClick={() => gotoEvent()}
+          >
+            {smartString(name, 50) || "..."}
+          </h6>
+        </div>
+      </div>
+      <div className="card-footer border-0 bg-transparent p-0">
         <Row>
           <Col className={"pe-0"}>
             <p className="text-sm fw-medium text-accent-3">
-              <span>{formatDate(start_date)}</span><span className={"text-dark"}> &mdash; </span>
+              <span>{formatDate(start_date)}</span>
+              <span className={"text-dark"}> &mdash; </span>
               <span>{formatDate(end_date)}</span>
             </p>
           </Col>
