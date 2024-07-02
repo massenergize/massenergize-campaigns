ebca4a987993de796076702569a8e5fe1fe50af4
frimpongopoku <pongofrimi@gmail.com>
2024-07-02 14:02:01 +0000

    added common languages offered

---
+++ src/utils/internationalization/languages.js
@@ -0,0 +1,112 @@
+export const LANGUAGES = [
+  {
+    languageName: "English",
+    languageISO: "en",
+    countryISOs: ["US", "GB", "CA", "AU", "NZ", "IE", "ZA"],
+  },
+  {
+    languageName: "Spanish",
+    languageISO: "es",
+    countryISOs: ["ES", "MX", "AR", "CO", "CL", "PE", "VE"],
+  },
+  {
+    languageName: "Chinese",
+    languageISO: "zh",
+    countryISOs: ["CN", "TW", "SG", "MY"],
+  },
+  {
+    languageName: "French",
+    languageISO: "fr",
+    countryISOs: ["FR", "CA", "BE", "CH", "LU"],
+  },
+  {
+    languageName: "German",
+    languageISO: "de",
+    countryISOs: ["DE", "AT", "CH", "LU", "LI"],
+  },
+  {
+    languageName: "Japanese",
+    languageISO: "ja",
+    countryISOs: ["JP"],
+  },
+  {
+    languageName: "Portuguese",
+    languageISO: "pt",
+    countryISOs: ["PT", "BR"],
+  },
+  {
+    languageName: "Russian",
+    languageISO: "ru",
+    countryISOs: ["RU", "BY", "KZ"],
+  },
+  {
+    languageName: "Arabic",
+    languageISO: "ar",
+    countryISOs: ["SA", "EG", "IQ", "SY", "JO", "AE", "MA"],
+  },
+  {
+    languageName: "Italian",
+    languageISO: "it",
+    countryISOs: ["IT", "CH", "SM", "VA"],
+  },
+  {
+    languageName: "Dutch",
+    languageISO: "nl",
+    countryISOs: ["NL", "BE", "SR"],
+  },
+  {
+    languageName: "Korean",
+    languageISO: "ko",
+    countryISOs: ["KR", "KP"],
+  },
+  {
+    languageName: "Turkish",
+    languageISO: "tr",
+    countryISOs: ["TR", "CY"],
+  },
+  {
+    languageName: "Hindi",
+    languageISO: "hi",
+    countryISOs: ["IN", "FJ"],
+  },
+  {
+    languageName: "Swedish",
+    languageISO: "sv",
+    countryISOs: ["SE", "FI"],
+  },
+  {
+    languageName: "Norwegian",
+    languageISO: "no",
+    countryISOs: ["NO"],
+  },
+  {
+    languageName: "Danish",
+    languageISO: "da",
+    countryISOs: ["DK"],
+  },
+  {
+    languageName: "Finnish",
+    languageISO: "fi",
+    countryISOs: ["FI"],
+  },
+  {
+    languageName: "Greek",
+    languageISO: "el",
+    countryISOs: ["GR", "CY"],
+  },
+  {
+    languageName: "Polish",
+    languageISO: "pl",
+    countryISOs: ["PL"],
+  },
+  {
+    languageName: "Czech",
+    languageISO: "cs",
+    countryISOs: ["CZ"],
+  },
+  {
+    languageName: "Hungarian",
+    languageISO: "hu",
+    countryISOs: ["HU"],
+  },
+];
