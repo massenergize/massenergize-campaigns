465af1c13909f16224d176aa53393652010ade70
frimpongopoku <pongofrimi@gmail.com>
2024-09-18 10:08:29 +0000

    footer notes

--- src/components/themes/spt/components/SPTFooter.js
+++ src/components/themes/spt/components/SPTFooter.js
@@ -3,19 +3,23 @@ import React from "react";
 function SPTFooter() {
   return (
     <div style={{ display: "flex", flexDirection: "row" }}>
-      <div
-        className="spt-footer spt-section-padding"
-   
-      >
+      <div className="spt-footer spt-section-padding">
         <h1>Chat or text with our ambassadors </h1>
 
         <div
           className="spt-btn mobile-width moblie-margin"
           style={{ "--my-custom-width": "fit-content", "--my-custom-margin": "30px 0px" }}
-         
         >
           Email Us!
         </div>
+
+        <small className="spt-footer-notes" style={{ marginTop: 20, color: "#b5b5b5" }}>
+          <span style={{ textDecoration: "underline", fontWeight: "bold" }}>Copyright &copy; 2024</span> All Rights
+          Reserved. Powered by{" "}
+          <span className="" style={{ textDecoration: "underline", fontWeight: "bold" }}>
+            MassEnergize
+          </span>
+        </small>
       </div>
       <div className="phone-vanish" style={{ width: "25%" }}>
         <img
