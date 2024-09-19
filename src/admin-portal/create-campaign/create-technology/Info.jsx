4bccc2662aaac0ec11e9aeeb699e80de03c842b6
Tahiru Abdullai <abdullai.tahiru@gmail.com>
2024-09-19 16:19:52 +0000

    Increase summary field character limit in campaign form
    Changed the character limit for the "Summary" field from 75 to 500 characters. This allows users to provide more detailed and comprehensive summaries.

--- src/admin-portal/create-campaign/create-technology/Info.jsx
+++ src/admin-portal/create-campaign/create-technology/Info.jsx
@@ -119,14 +119,14 @@ function Info({
         <Row className="mt-3">
           <Col>
             <Input
-              label="Summary (75 Chars)"
+              label="Summary"
               placeholder="Add a Summary for this focus......."
               required={true}
               type="textbox"
               onChange={(val) => {
                 handleFieldChange("summary", val);
               }}
-              maxLength="75"
+              maxLength="500"
               value={getValue("summary")}
             />
           </Col>
