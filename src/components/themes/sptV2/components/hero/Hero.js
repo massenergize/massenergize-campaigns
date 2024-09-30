2299c0cbdcf9918e80683b3676521f7da51c73a7
Tahiru Abdullai <abdullai.tahiru@gmail.com>
2024-09-30 16:44:10 +0000

    Remove hardcoded images and update hero content dynamically
    Refactored Hero component to dynamically fetch images and content from the campaign state, rather than using hardcoded values. This includes updating the hero title, tagline, and call-to-action button with campaign-specific data.

--- src/components/themes/sptV2/components/hero/Hero.js
+++ src/components/themes/sptV2/components/hero/Hero.js
@@ -11,7 +11,6 @@ import { useSelector } from "react-redux";
 
 const Hero = ({ themeKey }) => {
   const campaign = useSelector((state) => state.campaign);
-  console.log("This is the campaign", campaign);
   const settings = {
     dots: true,
     infinite: true,
@@ -21,16 +20,17 @@ const Hero = ({ themeKey }) => {
     autoplay: true,
     autoplaySpeed: 3000, //Waiting time for next slide
   };
+  const images = (campaign?.media ||[])?.map(item => item?.media?.url)
+  // const images = [
+  //   // "https://via.placeholder.com/1600x900/1975bb/fff?text=1975bb",
+  //   "https://massenergize-prod-files.s3.amazonaws.com/media/alternative-alternative-energy-clouds-eco-energy-433308.jpg",
+  //   // "https://via.placeholder.com/1600x900/ec4233/fff?text=ec4233",
+  //   "https://massenergize-prod-files.s3.amazonaws.com/media/clear-light-bulb-planter-on-gray-rock-1108572_1.jpg",
+  //   // "https://via.placeholder.com/1600x900/f2c044/fff?text=f2c044",
+  //   "https://massenergize-prod-files.s3.amazonaws.com/media/IMG_5734-231017-153600.JPG",
+  // ];
 
-  const images = [
-    // "https://via.placeholder.com/1600x900/1975bb/fff?text=1975bb",
-    "https://massenergize-prod-files.s3.amazonaws.com/media/alternative-alternative-energy-clouds-eco-energy-433308.jpg",
-    // "https://via.placeholder.com/1600x900/ec4233/fff?text=ec4233",
-    "https://massenergize-prod-files.s3.amazonaws.com/media/clear-light-bulb-planter-on-gray-rock-1108572_1.jpg",
-    // "https://via.placeholder.com/1600x900/f2c044/fff?text=f2c044",
-    "https://massenergize-prod-files.s3.amazonaws.com/media/IMG_5734-231017-153600.JPG",
-  ];
-
+  console.log("== log==", campaign)
   const theme = getTheme(themeKey);
   const { primary_logo } = campaign || {};
   return (
@@ -39,7 +39,7 @@ const Hero = ({ themeKey }) => {
         <img
           // style={{ objectFit: "contain", height: 70 }}
           src={primary_logo?.url}
-          alt="logo"
+          alt=""
         />
 
         <div className="pc-vanish" style={{ marginLeft: "auto" }}>
@@ -57,9 +57,9 @@ const Hero = ({ themeKey }) => {
           }}
         >
           <div className="hero-content">
-            <h1 className="hero-title">Solar Para Todos</h1>
-            <p className="hero-subtext">Save money with clean energy!</p>
-            <SPTButton themeKey={themeKey}>See if you qualify</SPTButton>
+            <h1 className="hero-title">{campaign?.title}</h1>
+            <p className="hero-subtext">{campaign?.tagline}</p>
+            <SPTButton themeKey={themeKey} href={campaign?.call_to_action?.url}>{campaign?.call_to_action?.text}</SPTButton>
             {/* <div
               className="spt-btn touchable-opacity custom-margin"
               style={{ "--my-custom-margin": "20px 0px" }}
