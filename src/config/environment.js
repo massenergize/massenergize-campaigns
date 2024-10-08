5cf8c3f95e8a189fca9b30a6edfcd751b41ef649
frimpongopoku <pongofrimi@gmail.com>
2024-10-08 15:11:30 +0000

    environment changes

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,9 +1,9 @@
 const NODE_ENV = process.env.NODE_ENV || 'local';
 const TARGET_ENV = process.env.REACT_APP_TARGET_ENV || 'development';
 
-console.log('TARGET_ENV', TARGET_ENV);
+console.log('TARGET_ENV, NODE_ENV', TARGET_ENV, NODE_ENV);
 
-export const IS_DEV = NODE_ENV === "production" && TARGET_ENV === 'development';
+export const IS_DEV = NODE_ENV === "development" && TARGET_ENV === 'development';
 export const IS_PROD = NODE_ENV === "production" && TARGET_ENV === 'production';
-export const IS_CANARY = NODE_ENV === "production" && TARGET_ENV === 'canary';
+export const IS_CANARY = NODE_ENV === "canary" && TARGET_ENV === 'canary';
 export const IS_LOCAL = NODE_ENV === "development";
