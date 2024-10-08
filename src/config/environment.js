847941916035a07dd724f52bf04848961d8a99ff
frimpongopoku <pongofrimi@gmail.com>
2024-10-08 15:22:39 +0000

    more changes to target envs

--- src/config/environment.js
+++ src/config/environment.js
@@ -3,7 +3,7 @@ const TARGET_ENV = process.env.REACT_APP_TARGET_ENV || 'development';
 
 console.log('TARGET_ENV, NODE_ENV', TARGET_ENV, NODE_ENV);
 
-export const IS_DEV = NODE_ENV === "development" && TARGET_ENV === 'development';
+export const IS_DEV = NODE_ENV === "production" && TARGET_ENV === 'development';
 export const IS_PROD = NODE_ENV === "production" && TARGET_ENV === 'production';
-export const IS_CANARY = NODE_ENV === "canary" && TARGET_ENV === 'canary';
+export const IS_CANARY = NODE_ENV === "production" && TARGET_ENV === 'canary';
 export const IS_LOCAL = NODE_ENV === "development";
