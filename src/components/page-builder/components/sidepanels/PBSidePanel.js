e917d6ff3e5c7b7a4a9812ca081b174a4e6570e4
frimpongopoku <pongofrimi@gmail.com>
2024-10-22 13:58:39 +0000

    push bttextbox

--- src/components/page-builder/components/sidepanels/PBSidePanel.js
+++ src/components/page-builder/components/sidepanels/PBSidePanel.js
@@ -1,7 +1,23 @@
 import React from "react";
 import "./pb-sidepanel.css";
 function PBSidePanel() {
-  return <div className="pb-side-panel-root"></div>;
+  return (
+    <div className="pb-side-panel-root">
+      <h6>Properties</h6>
+      <div className="flex-row align-center">
+        <div className="pb-textbox">
+          <label>Width</label>
+          <br />
+          <input type="number" />
+        </div>
+        <div className="pb-textbox" style={{ marginLeft: 10 }}>
+          <label>Height</label>
+          <br />
+          <input type="number" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default PBSidePanel;
